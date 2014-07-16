0b9ca31c343af9997645b2ec967f17475aca1451
Kyle Clarke <kyle@vendhq.com>
2014-07-17 07:11:48 +1200

    Initial new css style creation

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,10 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/blood.css': 'css/theme/source/blood.scss',
+
+					// Custom Themes
+					'css/theme/love.css': 'css/theme/source/love.scss'
 				}
 			}
 		},
